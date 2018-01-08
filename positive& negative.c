a30edad591e8c8d258a61d85def1c09eb5038ff4
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-01-08 16:33:48 +0530

    Create positive& negative.c

---
+++ positive& negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+double Number;
+printf("Enter the no is" );
+if(Number<=100000)
+{
+printf("Your enter the no");
+else
+printf("Your no is +ve");
+}
+else
+printf("Your no is _ve");
+return 0;
+}
